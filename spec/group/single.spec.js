6f02cd4ae85ecc85e33ff48c48c872de6abe4161
Alexy Maslennikov <alexy.maslennikov@gmail.com>
2013-01-06 16:47:42 +0400

    testing functionality of callback queuing

--- spec/group/single.spec.js
+++ spec/group/single.spec.js
@@ -2,13 +2,49 @@
 var G = require('../../lib/twoStep').Group,
     fs = require('fs');
 
+var selfText = fs.readFileSync(__filename, 'utf8');
+
 describe('Testing single group functionality', function() {
     it('Wrapping single async call', function(done) {
-        var selfText = fs.readFileSync(__filename, 'utf8');
         var group = new G(function(err, text) {
             expect(selfText).toEqual(text);
             done(err);
         });
         fs.readFile(__filename, 'utf8', group.slot());
+    });
+
+    it('Wrapping two concurrent async calls', function(done) {
+        var group = new G(function(err, text1, text2) {
+            expect(selfText).toEqual(text1);
+            expect(selfText).toEqual(text2);
+            done(err);
+        });
+        fs.readFile(__filename, 'utf8', group.slot());
+        group.pass(selfText);
+    });
+
+    it('Queuing several callbacks', function(done) {
+        var group = new G(function(err, text1, text2) {
+            expect(selfText).toEqual(text1);
+            expect(selfText).toEqual(text2);
+            done(err);
+        });
+
+        var group2 = new G();
+        fs.readFile(__filename, 'utf8', group2.slot());
+        group2.then(group.slot());
+        group2.then(group.slot());
+    });
+
+    it('Queuing callback of the resolved group', function(done) {
+        var group = new G();
+        fs.readFile(__filename, 'utf8', group.slot());
+        group.then(function(err, text) {
+            expect(group.resolved).toEqual(true);
+            group.then(function(err, text) {
+                expect(selfText).toEqual(text);
+                done(err);
+            });
+        });
     });
 });
