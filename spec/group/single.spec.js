90e59c31293afc3a3a2762c74ff6045c4ae198fc
Alexy Maslennikov <alexy.maslennikov@gmail.com>
2013-01-07 16:17:43 +0400

    Group registers callbacks only through `then()` call

--- spec/group/single.spec.js
+++ spec/group/single.spec.js
@@ -6,34 +6,36 @@ var selfText = fs.readFileSync(__filename, 'utf8');
 
 describe('Testing single group functionality', function() {
     it('Wrapping single async call', function(done) {
-        var group = new G(function(err, text) {
+        var group = new G();
+        fs.readFile(__filename, 'utf8', group.slot());
+        group.then(function(err, text) {
             expect(selfText).toEqual(text);
             done(err);
         });
-        fs.readFile(__filename, 'utf8', group.slot());
     });
 
     it('Wrapping two concurrent async calls', function(done) {
-        var group = new G(function(err, text1, text2) {
+        G.chain(function() {
+            fs.readFile(__filename, 'utf8', this.slot());
+            this.pass(selfText);
+        }).then(function(err, text1, text2) {
             expect(selfText).toEqual(text1);
             expect(selfText).toEqual(text2);
             done(err);
         });
-        fs.readFile(__filename, 'utf8', group.slot());
-        group.pass(selfText);
     });
 
     it('Queuing several callbacks', function(done) {
-        var group = new G(function(err, text1, text2) {
+        G.chain(function() {
+            var group2 = new G();
+            fs.readFile(__filename, 'utf8', group2.slot());
+            group2.then(this.slot());
+            group2.then(this.slot());
+        }).then(function(err, text1, text2) {
             expect(selfText).toEqual(text1);
             expect(selfText).toEqual(text2);
             done(err);
         });
-
-        var group2 = new G();
-        fs.readFile(__filename, 'utf8', group2.slot());
-        group2.then(group.slot());
-        group2.then(group.slot());
     });
 
     it('Queuing callback of the resolved group', function(done) {
