6052c5b96846f5422df6622678360906243eb22c
Alex Maslennikov <alexy.maslennikov@gmail.com>
2014-05-31 23:11:34 +0200

    Executing tests with tape

--- spec/flowy.spec.js
+++ spec/flowy.spec.js
@@ -1,136 +1,143 @@
 'use strict';
 
-var Flowy = require('../lib/flowy'),
+var test = require('tape'),
+    Flowy = require('../lib/flowy'),
     fs = require('fs');
 
-var standard = fs.readFileSync(__filename, 'utf8');
-function afun(callback) {
-    fs.readFile(__filename, 'utf8', callback);
+var kitty = {name: 'robert', gender: 'male'};
+
+function asyncKitty(callback/*(err, value)*/) {
+    process.nextTick(function() {
+        callback(null, kitty);
+    });
 }
-function echoAsync(val, callback) {
+
+function asyncEcho(val, callback) {
     process.nextTick(function() {
         callback(null, val);
     });
 }
 
-describe('Flowy usage', function() {
+test('calling step with less than two steps', function(assert) {
+    try {
+        Flowy();
+    } catch(e) {
+        assert.ok(e);
+        assert.end();
+    }
+});
 
-    it('calling step with less than two steps', function() {
-        try {
-            Flowy();
-        } catch(e) {
-            expect(e).toBeTruthy();
+test('simple function chaining', function(assert) {
+    assert.plan(5);
+
+    Flowy(
+        function() {
+            //parallel execution of async calls
+            asyncKitty(this.slot());
+            asyncEcho('hello kitty', this.slot());
+        },
+        function(err, val, hello) {
+            assert.equal(val, kitty);
+            assert.equal(hello, 'hello kitty');
+            this.pass(kitty.name.toUpperCase(), 'uppercasing');
+        },
+        function(err, text, whatWeHaveDone) {
+            assert.error(err);
+            assert.equal(text, kitty.name.toUpperCase());
+            assert.equal(whatWeHaveDone, 'uppercasing');
+            //breaking step flow
+        },
+        function() {
+            assert.fail('should not be there');
         }
-    });
+    );
+});
 
-    it('simple function chaining', function(done) {
-        Flowy(
-            function() {
-                //parallel execution of async calls
-                afun(this.slot());
-                echoAsync('timeouted', this.slot());
-            },
-            function(err, text, timeouted) {
-                expect(text).toBe(standard);
-                expect(timeouted).toBe('timeouted');
-                this.pass(text.toUpperCase(), 'uppercasing');
-            },
-            function(err, text, whatWeHaveDone) {
-                done(err);
-                expect(text).toBe(standard.toUpperCase());
-                expect(whatWeHaveDone).toBe('uppercasing');
-                //breaking step flow
-            },
-            function() {
-                done(new Error('should not be there'));
+test('should throw error and catch it in last callback', function(assert) {
+    var error = new Error("hello error!");
+    Flowy(
+        function() {
+            throw error;
+        },
+        function(err) {
+            assert.fail('should not be there');
+        },
+        function(err) {
+            assert.fail('should not be there');
+        },
+        function(err) {
+            assert.equal(err, error);
+            assert.end();
+        }
+    );
+});
+
+test('sync values are handled asynchronously', function(assert) {
+    Flowy(
+        function() {
+            asyncKitty(this.slot());
+            this.pass('test');
+
+            var group = this.slotGroup();
+            for (var i = 0; i < 5; i++) {
+                group.pass('test' + i);
             }
-        );
-    });
-
-    it('should throw error and catch it in last callback', function(done) {
-        var error = new Error("hello error!");
-        Flowy(
-            function() {
-                throw error;
-            },
-            function(err) {
-                done(new Error('should not be there'));
-            },
-            function(err) {
-                done(new Error('should not be there'));
-            },
-            function(err) {
-                expect(err).toEqual(error);
-                done();
+            for (var i = 5; i < 10; i++) {
+                asyncEcho('test' + i, group.slot());
             }
-        );
-    });
+        },
+        function(err, async, sync, group) {
+            assert.error(err);
+            assert.equal(async, kitty);
+            assert.equal(sync, 'test');
+            assert.equal(group.length, 10);
+            assert.equal(group[0], 'test0');
+            assert.equal(group[9], 'test9');
+            assert.end();
+        }
+    );
+});
 
-    it('sync values are handled asynchronously', function(done) {
-        Flowy(
-            function() {
-                afun(this.slot());
-                this.pass('test');
+test('simple combined function test', function(assert) {
+    var multifun = Flowy.compose(
+        function(err, val) {
+            asyncEcho(val, this.slot());
+        },
+        function(err, text) {
+            this.pass(text.toUpperCase());
+        }
+    );
 
-                var group = this.slotGroup();
-                for (var i = 0; i < 5; i++) {
-                    group.pass('test' + i);
-                }
-                for (var i = 5; i < 10; i++) {
-                    echoAsync('test' + i, group.slot());
-                }
-            },
-            function(err, async, sync, group) {
-                done(err);
-                expect(async).toBe(standard);
-                expect(sync).toBe('test');
-                expect(group.length).toBe(10);
-                expect(group[0]).toBe('test0');
-                expect(group[9]).toBe('test9');
-            }
-        );
-    });
+    Flowy(
+        function() {
+            asyncKitty(this.slot());
+        },
+        function(err, pet) {
+            multifun(pet.name, this.slot());
+        },
+        function(err, text) {
+            assert.error(err);
+            assert.equal(text, kitty.name.toUpperCase());
+            assert.end();
+        }
+    );
+});
 
-    it('simple combined function test', function(done) {
-        var multifun = Flowy.compose(
-            function(err, val) {
-                echoAsync(val, this.slot());
-            },
-            function(err, text) {
-                this.pass(text.toUpperCase());
-            }
-        );
+test('saving the context of the composed function', function(assert) {
+    var multifun = Flowy.compose(
+        function() {
+            this.pass(this.self);
+        },
+        function(err, self) {
+            assert.equal(this.self, self);
+            assert.equal(this.self, this.options.self);
+            this.pass(self);
+        }
+    );
 
-        Flowy(
-            function() {
-                afun(this.slot());
-            },
-            function(err, text) {
-                multifun(text, this.slot());
-            },
-            function(err, text) {
-                done(err);
-                expect(text).toBe(standard.toUpperCase());
-            }
-        );
-    });
-
-    it('saving the context of the composed function', function(done) {
-        var multifun = Flowy.compose(
-            function() {
-                this.pass(this.self);
-            },
-            function(err, self) {
-                expect(this.self).toBe(self);
-                expect(this.self).toBe(this.options.self);
-                this.pass(self);
-            }
-        );
-
-        var context = {message: 'hello'};
-        multifun.call(context, function(err, self) {
-            expect(self.message).toBe(context.message);
-            done(err);
-        });
+    var context = {message: 'hello'};
+    multifun.call(context, function(err, self) {
+        assert.equal(self.message, context.message);
+        assert.end(err);
     });
 });
--- spec/group.spec.js
+++ spec/group.spec.js
@@ -1,191 +1,209 @@
 'use strict';
-var G = require('../lib/flowy').Group,
+
+var test = require('tape'),
+    G = require('../lib/flowy').Group,
     fs = require('fs');
 
-var standard = fs.readFileSync(__filename, 'utf8');
-function afun(callback) {
-    fs.readFile(__filename, 'utf8', callback);
-}
-function afunMultiArgs(callback) {
-    fs.readFile(__filename, 'utf8', function(err, text) {
-        callback(err, text, __filename);
+
+var kitty = {name: 'robert', gender: 'male'};
+
+function asyncKitty(callback/*(err, value)*/) {
+    process.nextTick(function() {
+        callback(null, kitty);
     });
 }
 
-describe('Testing group functionality', function() {
-    it('Wrapping single async call', function(done) {
-        var group = new G();
-        afun(group.slot());
-        group.then(function(err, text) {
-            expect(text).toEqual(standard);
-            done(err);
-        });
+function asyncSpreadKitty(callback/*(err, name, gender)*/) {
+    process.nextTick(function() {
+        callback(null, kitty.name, kitty.gender);
     });
+}
 
-    it('Handle all response arguments of the async call', function(done) {
-        var group = new G();
-        afunMultiArgs(group.slot('multi'));
-        group.then(function(err, response) {
-            expect(response.length).toBe(2);
-            expect(response[0]).toBe(standard);
-            expect(response[1]).toBe(__filename);
-            done(err);
-        });
+
+test('Wrapping single async call', function(assert) {
+    var group = new G();
+    asyncKitty(group.slot());
+    group.then(function(err, val) {
+        assert.error(err);
+        assert.equal(val, kitty);
+        assert.end();
     });
+});
 
-    it('Starting chain with `when`', function(done) {
-        var error = new Error('hello');
+
+test('Handle all response arguments of the async call', function(assert) {
+    var group = new G();
+    asyncSpreadKitty(group.slot('multi'));
+    group.then(function(err, response) {
+        assert.error(err);
+        assert.equal(response.length, 2);
+        assert.equal(response[0], kitty.name);
+        assert.equal(response[1], kitty.gender);
+        assert.end();
+    });
+});
+
+test('Starting chain with `when`', function(assert) {
+    var error = new Error('hello');
+    G.when(
+        error
+    ).fail(function(err) {
+        assert.equal(err, error);
         G.when(
-            error
-        ).fail(function(err) {
-            expect(err).toBe(error);
-            G.when(
-                null, 'message'
-            ).anyway(function(err, message) {
-                expect(message).toBe('message');
-                done(err);
-            });
+            null, 'message'
+        ).anyway(function(err, message) {
+            assert.error(err);
+            assert.equal(message, 'message');
+            assert.end();
         });
     });
+});
 
-    it('Wrapping two concurrent async calls', function(done) {
-        G.chain(function() {
-            afun(this.slot());
-            this.pass(standard);
-        }).then(function(err, text1, text2) {
-            expect(text1).toEqual(standard);
-            expect(text2).toEqual(standard);
-            done(err);
+test('Wrapping two concurrent async calls', function(assert) {
+    G.chain(function() {
+        asyncKitty(this.slot());
+        this.pass(kitty);
+    }).then(function(err, val1, val2) {
+        assert.error(err);
+        assert.equal(val1, kitty);
+        assert.equal(val2, kitty);
+        assert.end();
+    });
+});
+
+test('Queuing several callbacks', function(assert) {
+    G.chain(function() {
+        var group2 = new G();
+        asyncKitty(group2.slot());
+        group2.then(this.slot());
+        group2.then(this.slot());
+    }).then(function(err, val1, val2) {
+        assert.error(err);
+        assert.equal(val1, kitty);
+        assert.equal(val2, kitty);
+        assert.end();
+    });
+});
+
+test('Queuing callback of the resolved group', function(assert) {
+    var group = new G();
+    asyncKitty(group.slot());
+    group.then(function(err, text) {
+        assert.equal(group.resolved, 'fulfilled');
+        group.then(function(err, val) {
+            assert.error(err);
+            assert.equal(val, kitty);
+            assert.end();
         });
     });
+});
 
-    it('Queuing several callbacks', function(done) {
-        G.chain(function() {
-            var group2 = new G();
-            afun(group2.slot());
-            group2.then(this.slot());
-            group2.then(this.slot());
-        }).then(function(err, text1, text2) {
-            expect(text1).toEqual(standard);
-            expect(text2).toEqual(standard);
-            done(err);
-        });
+test('Wrapping a function in the group-sandbox', function(assert) {
+    var promise = new G().fbind(function(val) {
+        this.pass(val);
+        asyncKitty(this.slot());
     });
-
-    it('Queuing callback of the resolved group', function(done) {
-        var group = new G();
-        afun(group.slot());
-        group.then(function(err, text) {
-            expect(group.resolved).toEqual('fulfilled');
-            group.then(function(err, text) {
-                expect(text).toEqual(standard);
-                done(err);
-            });
-        });
+    promise('test').then(function(err, text, val) {
+        assert.error(err);
+        assert.equal(text, 'test');
+        assert.equal(val, kitty);
+        assert.end();
     });
+});
 
-    it('Wrapping a function in the group-sandbox', function(done) {
-        var promise = new G().fbind(function(val) {
-            this.pass(val);
-            afun(this.slot());
-        });
-        promise('test').then(function(err, val, text) {
-            expect(val).toBe('test');
-            expect(text).toBe(standard);
-            done(err);
-        });
+test('resolving a group with empty args', function(assert) {
+    new G().resolve().anyway(assert.end);
+});
+
+test('erroring a group', function(assert) {
+    new G().error('hello').anyway(function(err) {
+        assert.equal(err, 'hello');
+        assert.end();
     });
+});
 
-    it('resolving a group with empty args', function(done) {
-        new G().resolve().anyway(done);
+test('Propagating value if no callback given', function(assert) {
+    var error = new Error('Hello error');
+    G.chain(function() {
+        asyncKitty(this.slot());
+    }).then(null, function(err) {
+        assert.fail('never should be called');
+    }).anyway(function(err, val) {
+        assert.error(err);
+        assert.equal(val, kitty);
+        assert.end();
     });
+});
 
-    it('erroring a group', function(done) {
-        new G().error('hello').anyway(function(err) {
-            expect(err).toBe('hello');
-            done();
-        });
+test('Propagating error without errbacks', function(assert) {
+    var error = new Error('Hello error');
+    G.chain(function() {
+        throw error;
+    }).then(function(err) {
+        assert.fail('never should be called');
+    }).anyway(function(err) {
+        assert.equal(err, error);
+        assert.end();
     });
+});
 
-    it('Propagating value if no callback given', function(done) {
-        var error = new Error('Hello error');
-        G.chain(function() {
-            afun(this.slot());
-        }).then(null, function(err) {
-            //never should be called
-            expect(1).toBe(2);
-        }).anyway(function(err, text) {
-            expect(text).toBe(standard);
-            done(err);
-        });
+test('Handling errors in the middle', function(assert) {
+    var error = new Error('Hello error');
+    G.chain(function() {
+        throw error;
+    }).then(function(err) {
+        assert.fail('never should be called');
+    }).then(null, function(err) {
+        assert.ok(err);
+        asyncKitty(this.slot());
+    }).anyway(function(err, val) {
+        assert.error(err);
+        assert.equal(val, kitty);
+        assert.end();
     });
+});
 
-    it('Propagating error without errbacks', function(done) {
-        var error = new Error('Hello error');
-        G.chain(function() {
-            throw error;
-        }).then(function(err) {
-            //never should be called
-            expect(1).toBe(2);
-        }).anyway(function(err) {
-            expect(err).toBe(error);
-            done();
+test('Creating dependent group via slotGroup', function(assert) {
+    G.chain(function() {
+        this.pass('pass');
+        asyncKitty(this.slot());
+        this.slotGroup().fcall(function() {
+            this.pass('pass2');
+            asyncKitty(this.slot());
         });
+    }).anyway(function(err, sync, async, group) {
+        assert.error(err);
+        assert.equal(sync, 'pass');
+        assert.equal(async, kitty);
+        assert.equal(group[0], 'pass2');
+        assert.equal(group[1], kitty);
+        assert.end();
     });
+});
 
-    it('Handling errors in the middle', function(done) {
-        var error = new Error('Hello error');
-        G.chain(function() {
-            throw error;
-        }).then(function(err) {
-            //never should be called
-            expect(1).toBe(2);
-        }).then(null, function(err) {
-            afun(this.slot());
-        }).anyway(function(err, text) {
-            expect(text).toBe(standard);
-            done(err);
-        });
+test('Sharing the same `options` object through the chain', function(assert) {
+    new G({message: 'hello'}).fcall(function() {
+        this.pass(this.options.message);
+    }).then(function(err, message) {
+        assert.error(err);
+        assert.equal(this.options.message, message);
+        this.options.response = 'hi';
+        this.pass(this.options.response);
+        this.options = {}; //should have no effect
+    }).anyway(function(err, response) {
+        assert.error(err);
+        assert.equal(this.options.response, response);
+        assert.end();
     });
+});
 
-    it('Creating dependent group via slotGroup', function(done) {
-        G.chain(function() {
-            this.pass('pass');
-            afun(this.slot());
-            this.slotGroup().fcall(function() {
-                this.pass('pass2');
-                afun(this.slot());
-            });
-        }).anyway(function(err, sync, async, group) {
-            expect(sync).toBe('pass');
-            expect(async).toBe(standard);
-            expect(group[0]).toBe('pass2');
-            expect(group[1]).toBe(standard);
-            done(err);
-        });
-    });
-
-    it('Sharing the same `options` object through the chain', function(done) {
-        new G({message: 'hello'}).fcall(function() {
-            this.pass(this.options.message);
-        }).then(function(err, message) {
-            expect(this.options.message).toBe(message);
-            this.options.response = 'hi';
-            this.pass(this.options.response);
-            this.options = {}; //should have no effect
-        }).anyway(function(err, response) {
-            expect(this.options.response).toBe(response);
-            done(err);
-        });
-    });
-
-    it('Shortcut `this.self` for `this.options.self`', function(done) {
-        new G({self: {message: 'hello'}}).fcall(function() {
-            expect(this.self.message).toBe(this.options.self.message);
-            this.pass(this.self);
-        }).anyway(function(err, self) {
-            expect(this.self.message).toBe(self.message);
-            done(err);
-        });
+test('Shortcut `this.self` for `this.options.self`', function(assert) {
+    new G({self: {message: 'hello'}}).fcall(function() {
+        assert.equal(this.self.message, this.options.self.message);
+        this.pass(this.self);
+    }).anyway(function(err, self) {
+        assert.error(err);
+        assert.equal(this.self.message, self.message);
+        assert.end();
     });
 });
